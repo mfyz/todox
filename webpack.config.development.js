e3376148be586432eaf48c0d7f986126a9905693
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-06-06 15:14:32 -0700

    changes

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,31 +1,32 @@
 /* eslint strict: 0 */
+
 'use strict';
 
-var webpack = require('webpack');
-var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
-var baseConfig = require('./webpack.config.base');
+let webpack = require('webpack');
+let webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
+let baseConfig = require('./webpack.config.base');
 
 
-var config = Object.create(baseConfig);
+let config = Object.create(baseConfig);
 
 config.debug = true;
 
 config.devtool = 'cheap-module-eval-source-map';
 
 config.entry = [
-  'webpack-hot-middleware/client?path=http://localhost:5849/__webpack_hmr',
-  './app/mainApp'
+	'webpack-hot-middleware/client?path=http://localhost:5849/__webpack_hmr',
+	'./app/mainApp'
 ];
 
 config.output.publicPath = 'http://localhost:5849/dist/';
 
 config.plugins.push(
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
-  new webpack.DefinePlugin({
-    __DEV__: true,
-    'process.env': JSON.stringify('development')
-  })
+	new webpack.HotModuleReplacementPlugin(),
+	new webpack.NoErrorsPlugin(),
+	new webpack.DefinePlugin({
+		__DEV__: true,
+		'process.env': JSON.stringify('development')
+	})
 );
 
 config.target = webpackTargetElectronRenderer(config);
